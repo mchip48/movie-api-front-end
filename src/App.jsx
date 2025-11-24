1e6d413d7d5040fa57b37f3da42c374bce4daf3f
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-24 15:24:02 -0600

    Added imports to App.jsx fo Footer, Header, and ActorsPage files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { ActorsPage } from "./ActorsPage";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
-  
-  </>
+    <div>
+      <Header />
+      <ActorsPage />
+      <Footer />
+    </div>
   )
+}
+
+function Header() {
+  return (
+    <header>
+      <a href='#'>Home</a> | <a href='#actors-index'>All actors</a> | <a href='#actors-new'>New Actor</a>
+    </header>
+  );
 }
 
 export default App
